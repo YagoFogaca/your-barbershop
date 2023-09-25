8f82ff23a8d506546b13e1377bc57811b4370bff
YagoFogaca <yagofogaca20@gmail.com>
2023-09-25 15:17:02 -0300

    feat: configuração do vite e implementação das dependências bootstrap, bootstrap-icons e jquery

--- server/resources/js/bootstrap.js
+++ server/resources/js/bootstrap.js
@@ -4,10 +4,14 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+import axios from "axios";
+import $ from "jquery";
+import * as bootstrap from "bootstrap";
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.bootstrap = bootstrap;
+window.$ = $;
+window.axios = axios;
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
