ee92eea590703e3425d50194019d2a027ed0dafb
YagoFogaca <yagofogaca20@gmail.com>
2023-09-25 18:41:20 -0300

    feat: modelagem dos dados de usuários

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->ulid('id');
+            $table->string('owner_name'); // nome do proprietário
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('telephone')->unique();
+            $table->string('company_name')->unique(); // nome do salão/barbearia
+            $table->string('cep')->nullable();
+            $table->string('state')->nullable(); // estado,
+            $table->string('city')->nullable(); // cidade,
+            $table->string('neighborhood')->nullable(); // bairro,
+            $table->string('address')->nullable(); // endereço,
+            $table->string('number')->nullable(); // numero,
+            $table->string('complement')->nullable(); // complemento,
             $table->rememberToken();
             $table->timestamps();
         });
