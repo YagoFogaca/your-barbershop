7eada9e7ef211dac94c50525fc1a266bef079d02
YagoFogaca <yagofogaca20@gmail.com>
2023-09-27 16:48:58 -0300

    feat: modularização da função de validar inputs e liberar botões

---
+++ server/resources/js/utils/check-form-validity.js
@@ -0,0 +1,56 @@
+export function checkFormValidity(process) {
+    switch (process) {
+        case 1:
+            const ownerNameValid =
+                $('input[name="owner_name"]').val().length > 3;
+            const companyNameValid =
+                $('input[name="company_name"]').val().length > 2;
+            const emailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(
+                $('input[name="email"]').val()
+            );
+            if (ownerNameValid && companyNameValid && emailValid) {
+                $("#continue").prop("disabled", false);
+            } else {
+                $("#continue").prop("disabled", true);
+            }
+            break;
+        case 2:
+            const telephoneValid =
+                $('input[name="telephone"]').val().trim() === "";
+            const cepValid = $('input[name="cep"]').val().trim() === "";
+            const addressValid = $('input[name="address"]').val().trim() === "";
+            const neighborhoodValid =
+                $('input[name="neighborhood"]').val().trim() === "";
+            const cityValid = $('input[name="city"]').val().trim() === "";
+            const stateValid = $('input[name="state"]').val().trim() === "";
+            const numberValid = $('input[name="number"]').val().trim() === "";
+
+            if (
+                !telephoneValid &&
+                !cepValid &&
+                !addressValid &&
+                !neighborhoodValid &&
+                !cityValid &&
+                !stateValid &&
+                !numberValid
+            ) {
+                $("#continue").prop("disabled", false);
+            } else {
+                $("#continue").prop("disabled", true);
+            }
+            break;
+
+        case 3:
+            const passwordValid = $('input[name="password"]').val().length > 7;
+            const passwordConfirmationValid =
+                $('input[name="password_confirmation"]').val() ===
+                $('input[name="password"]').val();
+
+            if (passwordValid && passwordConfirmationValid) {
+                $("#continue").prop("disabled", false);
+            } else {
+                $("#continue").prop("disabled", true);
+            }
+            break;
+    }
+}
--- server/resources/js/utils/validate-input.js
+++ server/resources/js/utils/validate-input.js
@@ -1,3 +1,5 @@
+import { checkFormValidity } from "./check-form-validity";
+
 export function validateInput(inputElement, minLength, regexPattern, process) {
     $(inputElement).on("input", function (event) {
         const value = event.target.value;
@@ -22,48 +24,4 @@ export function validateInput(inputElement, minLength, regexPattern, process) {
 
         checkFormValidity(process);
     });
-}
-
-function checkFormValidity(process) {
-    switch (process) {
-        case 1:
-            const ownerNameValid =
-                $('input[name="owner_name"]').val().length > 3;
-            const companyNameValid =
-                $('input[name="company_name"]').val().length > 2;
-            const emailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(
-                $('input[name="email"]').val()
-            );
-            if (ownerNameValid && companyNameValid && emailValid) {
-                $("#continue").prop("disabled", false);
-            } else {
-                $("#continue").prop("disabled", true);
-            }
-            break;
-        case 2:
-            const telephoneValid =
-                $('input[name="telephone"]').val().trim() === "";
-            const cepValid = $('input[name="cep"]').val().trim() === "";
-            const addressValid = $('input[name="address"]').val().trim() === "";
-            const neighborhoodValid =
-                $('input[name="neighborhood"]').val().trim() === "";
-            const cityValid = $('input[name="city"]').val().trim() === "";
-            const stateValid = $('input[name="state"]').val().trim() === "";
-            const numberValid = $('input[name="number"]').val().trim() === "";
-
-            if (
-                !telephoneValid &&
-                !cepValid &&
-                !addressValid &&
-                !neighborhoodValid &&
-                !cityValid &&
-                !stateValid &&
-                !numberValid
-            ) {
-                $("#continue").prop("disabled", false);
-            } else {
-                $("#continue").prop("disabled", true);
-            }
-            break;
-    }
 }
