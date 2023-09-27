d789f0bf618f1f7e2c2228fe3ff098d9f86c834f
YagoFogaca <yagofogaca20@gmail.com>
2023-09-27 15:37:08 -0300

    style: aplicação do estilo para suporte de blocos de inputs

--- server/resources/views/pages/user-create/index.blade.php
+++ server/resources/views/pages/user-create/index.blade.php
@@ -13,10 +13,11 @@
         </article>
 
         <article class="form">
-            <form name="create-user" class="row g-3 needs-validation">
+            <form name="create-user" class="needs-validation">
                 <div class="invalid-feedback" id="invalid-create-user">
 
                 </div>
+
                 <div class="process-block active-process-block">
                     <div class="mb-3 has-validation">
                         <label for="owner_name" class="form-label">Nome do Proprietário</label>
@@ -46,8 +47,86 @@
                     </div>
                 </div>
 
+                <div class="process-block row g-3">
+
+                    <div class="mb-3">
+                        <label for="telephone" class="form-label">Celular</label>
+                        <input type="tel" class="form-control" id="telephone" aria-describedby="telephone_user"
+                            name="telephone" placeholder="(XX)X XXXX-XXXX" required>
+                        <div class="invalid-feedback">
+                            Número de celular inválido.
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="cep" class="form-label">CEP</label>
+                        <input type="text" class="form-control" id="cep" aria-describedby="cep_user" name="cep"
+                            placeholder="00000-000">
+                        <div class="invalid-feedback">
+                            Número do CEP inválido.
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="address" class="form-label">Endereço</label>
+                        <input type="text" class="form-control" id="address" aria-describedby="address_user"
+                            name="address" placeholder="Rua x">
+                        <div id="address_user" class="form-text">Informe a sua Rua.</div>
+                    </div>
+
+                    <div class="mb-3 col-md-6">
+                        <label for="number" class="form-label">Número</label>
+                        <input type="text" class="form-control" id="number" aria-describedby="number_user"
+                            name="number" placeholder="000">
+                        <div class="invalid-feedback">
+                            Número da Barbearia/Salão.
+                        </div>
+                    </div>
+
+                    <div class="mb-3 col-md-6">
+                        <label for="neighborhood" class="form-label">Bairro</label>
+                        <input type="text" class="form-control" id="neighborhood" aria-describedby="neighborhood_user"
+                            name="neighborhood" placeholder="Bairro Barbearia/Salão">
+                    </div>
+
+                    <div class="mb-3 col-md-6">
+                        <label for="city" class="form-label">Cidade</label>
+                        <input type="text" class="form-control" id="city" aria-describedby="city_user"
+                            name="city" placeholder="Cidade Barbearia/Salão">
+                    </div>
+
+                    <div class="mb-3 col-md-6">
+                        <label for="state" class="form-label">Estado</label>
+                        <input type="text" class="form-control" id="state" aria-describedby="state_user"
+                            name="state" placeholder="Estado Barbearia/Salão">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="complement" class="form-label">Complemento (opcional)</label>
+                        <input type="text" class="form-control" id="complement" aria-describedby="complement_user"
+                            name="complement" placeholder="Estado Barbearia/Salão">
+                    </div>
+
+                </div>
+
+                <div class="process-block">
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Senha</label>
+                        <input type="password" class="form-control" id="password" aria-describedby="password_user"
+                            name="password" placeholder="8 digitos">
+                        <div id="password_user" class="form-text">Escolhe com cuidado.</div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="confirm_password" class="form-label">Confirme Senha</label>
+                        <input type="password" class="form-control" id="confirm_password"
+                            aria-describedby="password_user" name="confirm_password" placeholder="8 digitos">
+                        <div id="confirm_password_user" class="form-text">Escolhe com cuidado.</div>
+                    </div>
+                </div>
+
                 <div class="d-grid gap-2">
-                    <button type="submit" disabled class="btn btn-primary" id="continue">Continuar</button>
+                    <button type="button" disabled class="btn btn-primary" id="continue">Continuar</button>
                     <button class="btn btn-primary" type="button" disabled id="loading">
                         <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
                         <span role="status">Loading...</span>
